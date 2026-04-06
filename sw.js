dcdd94e10db20186e59cf11083acf5de15964552
FelipeBertevello <89105353+Berteva@users.noreply.github.com>
2026-04-05 21:12:05 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,16 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('financa-v1').then(cache => 
-    cache.addAll(['/', 'index.html'])
-  ));
+const CACHE = 'financa-v1';
+const urlsToCache = ['/', 'index.html', 'manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
